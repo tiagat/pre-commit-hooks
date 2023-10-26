635c8507ce200e473cca91f9c39429d1e249e2ed
Ruslan Sakal <ruslan.sakal@gmail.com>
2023-10-26 14:37:08 +0200

    initial commit

---
+++ check_aws_profile/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def print_arguments(arguments: list[str]):
+    for argument in arguments:
+        print(argument)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*")
+    args = parser.parse_args()
+
+    print_arguments(args.filenames)
+
+
+if __name__ == "__main__":
+    main()
